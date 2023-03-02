7ea7b5a1834c45914f068261669bf40679887764
eiurrutia <eiurrutia@uc.cl>
2023-03-02 18:56:27 +0100

    test: sku historic sales in same day of the week and same month

--- test/test_feature_engineering.py
+++ test/test_feature_engineering.py
@@ -154,6 +154,110 @@ def test_add_sku_historic_sales():
         ][f'sku_historic_sales'].values[0] == 27
 
 
+def test_add_sku_historic_sales_same_day_of_the_week():
+    data = {'sku': [
+                "1", "1", "2", "2", "2", "2",
+                "2", "4", "4", "4", "4", "4"
+            ],
+            'quantity': [5, 6, 7, 8, 4, 3,
+                         5, 6, 3, 8, 10, 11],
+            'date': [
+                '08-02-2023', '15-02-2023',
+                '09-02-2023', '23-02-2023',
+                '02-02-2023', '09-02-2023',
+                '16-02-2023', '03-02-2023',
+                '10-02-2023', '17-02-2023',
+                '24-02-2023', '03-03-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'COSTANERA',
+                'LASCONDES', 'LASCONDES',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'COSTANERA',
+                'LADEHESA', 'MALLSPORT',
+                'TEMUCO', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_sku_historic_sales_same_day_of_the_week(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-08')
+        ][f'sku_historic_sales_same_day_of_the_week'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'COSTANERA') &
+            (df['date'] == '2023-02-15')
+        ][f'sku_historic_sales_same_day_of_the_week'].values[0] == 5
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-09')
+        ][f'sku_historic_sales_same_day_of_the_week'].values[0] == 4
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-17')
+        ][f'sku_historic_sales_same_day_of_the_week'].values[0] == 9
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-10')
+        ][f'sku_historic_sales_same_day_of_the_week'].values[0] == 6
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-03-03')
+        ][f'sku_historic_sales_same_day_of_the_week'].values[0] == 27
+
+
+def test_add_sku_historic_sales_same_month():
+    data = {'sku': ["1", "1", "1", "2", "2", "2"],
+            'quantity': [100, 200, 80, 50, 70, 120],
+            'date': [
+                '08-02-2018', '12-02-2023',
+                '27-02-2023', '09-12-2019',
+                '09-12-2022', '09-12-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LADHESA',
+                'COSTANERA', 'TEMUCO',
+                'MALLSPORT', 'PUCON'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_sku_historic_sales_same_month(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'COSTANERA') &
+            (df['date'] == '2023-02-27')
+        ]['sku_historic_sales_same_month'].values[0] == 100
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2022-12-09')
+        ]['sku_historic_sales_same_month'].values[0] == 50
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'PUCON') &
+            (df['date'] == '2023-12-09')
+        ]['sku_historic_sales_same_month'].values[0] == 120
+
+
 def test_add_sku_warehouse_historic_sales():
     data = {'sku': [
                 "1", "1", "2", "2", "2", "2",
