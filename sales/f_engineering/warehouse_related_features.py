b77104a366243ce221da2b238cf1a95c7874b942
eiurrutia <eiurrutia@uc.cl>
2023-03-08 13:43:09 +0100

    feat: warehouse cumulative sales in the month

--- sales/f_engineering/warehouse_related_features.py
+++ sales/f_engineering/warehouse_related_features.py
@@ -101,3 +101,45 @@ def add_warehouse_cumulative_sales_in_the_week(df):
     result['warehouse_cumulative_sales_in_the_week'] = \
         result['warehouse_cumulative_sales_in_the_week'].astype('int64')
     return result
+
+
+def add_warehouse_cumulative_sales_in_the_month(df):
+    """
+    Adds a new column to the input DataFrame with the cumulative month
+    sales for the warehouse considering all skus.
+
+    Parameters:
+        --------
+        df: DataFrame
+            The input DataFrame with the sales data.
+        --------
+    Returns:
+        DataFrame: The input DataFrame with the new column added with the
+            name 'warehouse_cumulative_sales_in_the_month'
+    """
+    query = f'''
+        WITH cumulative_sales AS (
+            SELECT *,
+            COALESCE(SUM(quantity)
+            OVER (
+                PARTITION BY warehouse, strftime('%m-%Y', date)
+                ORDER BY date
+                ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
+            ), 0) AS warehouse_cumulative_sales_in_the_month
+            FROM (
+                SELECT warehouse, date, SUM(quantity) AS quantity
+                FROM df
+                GROUP BY warehouse, date
+            )
+        )
+        SELECT df.*,
+            cumulative_sales.warehouse_cumulative_sales_in_the_month
+        FROM df, cumulative_sales
+        WHERE df.date = cumulative_sales.date
+            AND df.warehouse = cumulative_sales.warehouse
+    '''
+    result = ps.sqldf(query)
+    result['date'] = pd.to_datetime(result['date'])
+    result['warehouse_cumulative_sales_in_the_month'] = \
+        result['warehouse_cumulative_sales_in_the_month'].astype('int64')
+    return result
