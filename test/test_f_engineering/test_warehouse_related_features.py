f3bc33aad332c7f0ecbe018ae63b06df82a873bc
eiurrutia <eiurrutia@uc.cl>
2023-03-08 13:43:18 +0100

    test: warehouse cumulative sales in the month

--- test/test_f_engineering/test_warehouse_related_features.py
+++ test/test_f_engineering/test_warehouse_related_features.py
@@ -166,3 +166,67 @@ def test_add_warehouse_cumulative_sales_in_the_week():
             (df['warehouse'] == 'MALLSPORT') &
             (df['date'] == '2023-02-09')
         ]['warehouse_cumulative_sales_in_the_week'].values[0] == 70
+
+
+def test_add_warehouse_cumulative_sales_in_the_month():
+    data = {'sku': ["1", "2", "3", "4", "5", "6", "7", "8"],
+            'quantity': [100, 200, 80, 50, 70, 120, 80, 25],
+            'date': [
+                '08-02-2018', '12-02-2023',
+                '27-02-2023', '28-02-2023',
+                '09-11-2022', '09-12-2023',
+                '18-12-2023', '29-12-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LADEHESA', 'LADEHESA',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_warehouse_cumulative_sales_in_the_month(df)
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-12')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '3') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-27')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-28')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 80
+    assert \
+        df[
+            (df['sku'] == '5') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2022-11-09')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '6') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-12-09')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '7') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-12-18')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 120
+    assert \
+        df[
+            (df['sku'] == '8') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-12-29')
+        ]['warehouse_cumulative_sales_in_the_month'].values[0] == 200
