df9a5c83f0dee6a52266d58ae5ce775445d0efe3
eiurrutia <eiurrutia@uc.cl>
2023-03-09 17:13:37 +0100

    test: company related features

---
+++ test/test_feature_engineering/test_company_related_features.py
@@ -0,0 +1,433 @@
+import pytest
+import pandas as pd
+from sales.data_holder import DataHolder
+from sales.feature_engineering.company_related_features import *
+
+
+def test_add_company_last_xdays_sales():
+    data = {'sku': ["1", "1", "1", "2", "2", "4", "4", "2", "2"],
+            'quantity': [5, 6, 7, 8, 4, 3, 5, 6, 3],
+            'date': [
+                '08-02-2023', '06-02-2023',
+                '07-02-2023', '08-02-2023',
+                '19-02-2023', '08-02-2023',
+                '07-02-2023', '02-02-2023',
+                '09-02-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LASCONDES', 'MALLSPORT',
+                'MALLSPORT', 'LADEHESA',
+                'COSTANERA', 'MALLSPORT',
+                'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+    days = 7
+
+    df = add_company_last_xdays_sales(df, days)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-08')
+        ][f'company_last_{days}days_sales'].values[0] == 24
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-09')
+        ][f'company_last_{days}days_sales'].values[0] == 40
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-19')
+        ][f'company_last_{days}days_sales'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-08')
+        ][f'company_last_{days}days_sales'].values[0] == 24
+
+
+def test_add_company_last_xdays_mean_sales():
+    data = {'sku': ["1", "1", "1", "2", "2", "2", "2", "2"],
+            'quantity': [5, 6, 7, 8, 4, 7, 2, 5],
+            'date': [
+                '08-02-2023', '06-02-2023',
+                '07-02-2023', '08-02-2023',
+                '19-02-2023', '22-02-2023',
+                '23-02-2023', '24-02-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LASCONDES', 'MALLSPORT',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+    days = 7
+
+    df = add_company_last_xdays_mean_sales(df, days)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-08')
+        ][f'company_last_{days}days_mean_sales'
+          ].values[0] == round(float(13/days), 4)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-07')
+        ][f'company_last_{days}days_mean_sales'
+          ].values[0] == round(float(6/days), 4)
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-19')
+        ][f'company_last_{days}days_mean_sales'
+          ].values[0] == round(float(0/days), 4)
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-08')
+        ][f'company_last_{days}days_mean_sales'
+          ].values[0] == round(float(13/days), 4)
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-24')
+        ][f'company_last_{days}days_mean_sales'
+          ].values[0] == round(float(13/days), 4)
+
+
+def test_add_company_historic_sales():
+    data = {'sku': [
+                "1", "1", "2", "2", "2", "2",
+                "2", "4", "4", "4", "4", "4", "2"
+            ],
+            'quantity': [5, 6, 7, 8, 4, 3,
+                         5, 6, 3, 8, 10, 11, 5],
+            'date': [
+                '08-02-2023', '15-03-2023',
+                '09-02-2023', '23-02-2023',
+                '02-05-2023', '09-08-2023',
+                '16-02-2023', '03-02-2023',
+                '10-02-2023', '17-02-2023',
+                '24-02-2023', '03-03-2023',
+                '23-02-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LASCONDES', 'LASCONDES',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'MALLSPORT',
+                'COSTANERA'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_company_historic_sales(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-03-15')
+        ][f'company_historic_sales'].values[0] == 68
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-23')
+        ][f'company_historic_sales'].values[0] == 34
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'COSTANERA') &
+            (df['date'] == '2023-02-23')
+        ][f'company_historic_sales'].values[0] == 34
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-08-09')
+        ][f'company_historic_sales'].values[0] == 78
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-03-03')
+        ][f'company_historic_sales'].values[0] == 57
+
+
+def test_add_company_historic_sales_same_day_of_the_week():
+    data = {'sku': [
+                "1", "1", "2", "2", "2", "2",
+                "2", "4", "4", "4", "4", "4"
+            ],
+            'quantity': [5, 6, 7, 8, 4, 3,
+                         5, 6, 3, 8, 10, 11],
+            'date': [
+                '08-02-2023', '15-02-2023',
+                '09-02-2023', '23-02-2023',
+                '02-02-2023', '09-02-2023',
+                '16-02-2023', '03-02-2023',
+                '10-02-2023', '17-02-2023',
+                '24-02-2023', '03-03-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'COSTANERA',
+                'LASCONDES', 'LASCONDES',
+                'MALLSPORT', 'MALLSPORT',
+                'MALLSPORT', 'COSTANERA',
+                'LADEHESA', 'MALLSPORT',
+                'TEMUCO', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_company_historic_sales_same_day_of_the_week(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-08')
+        ][f'company_historic_sales_same_day_of_the_week'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'COSTANERA') &
+            (df['date'] == '2023-02-15')
+        ][f'company_historic_sales_same_day_of_the_week'].values[0] == 5
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-09')
+        ][f'company_historic_sales_same_day_of_the_week'].values[0] == 4
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-17')
+        ][f'company_historic_sales_same_day_of_the_week'].values[0] == 9
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-10')
+        ][f'company_historic_sales_same_day_of_the_week'].values[0] == 6
+    assert \
+        df[
+            (df['sku'] == '4') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-03-03')
+        ][f'company_historic_sales_same_day_of_the_week'].values[0] == 27
+
+
+def test_add_company_historic_sales_same_month():
+    data = {'sku': ["1", "1", "1", "2", "2", "2"],
+            'quantity': [100, 200, 80, 50, 70, 120],
+            'date': [
+                '08-02-2018', '12-02-2023',
+                '27-02-2023', '09-12-2019',
+                '09-12-2022', '09-12-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LADHESA',
+                'COSTANERA', 'TEMUCO',
+                'MALLSPORT', 'PUCON'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_company_historic_sales_same_month(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'COSTANERA') &
+            (df['date'] == '2023-02-27')
+        ]['company_historic_sales_same_month'].values[0] == 100
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2022-12-09')
+        ]['company_historic_sales_same_month'].values[0] == 50
+    assert \
+        df[
+            (df['sku'] == '2') &
+            (df['warehouse'] == 'PUCON') &
+            (df['date'] == '2023-12-09')
+        ]['company_historic_sales_same_month'].values[0] == 120
+
+
+def test_add_company_cumulative_sales_in_the_week():
+    data = {'sku': ["1", "1", "1", "1", "1", "1"],
+            'quantity': [100, 200, 80, 50, 70, 120],
+            'date': [
+                '05-02-2023', '06-02-2023',
+                '12-02-2023', '14-02-2023',
+                '08-02-2023', '09-02-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LADEHESA', 'LADEHESA',
+                'MALLSPORT', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_company_cumulative_sales_in_the_week(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-06')
+        ]['company_cumulative_sales_in_the_week'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-12')
+        ]['company_cumulative_sales_in_the_week'].values[0] == 390
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-14')
+        ]['company_cumulative_sales_in_the_week'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-08')
+        ]['company_cumulative_sales_in_the_week'].values[0] == 200
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-02-09')
+        ]['company_cumulative_sales_in_the_week'].values[0] == 270
+
+
+def test_add_company_cumulative_sales_in_the_month():
+    data = {'sku': ["1", "1", "1", "1", "1", "1"],
+            'quantity': [100, 200, 80, 50, 70, 120],
+            'date': [
+                '08-02-2018', '12-02-2023',
+                '27-02-2023', '28-02-2023',
+                '09-11-2022', '09-12-2023'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LADEHESA', 'LADEHESA',
+                'MALLSPORT', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_company_cumulative_sales_in_the_month(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-12')
+        ]['company_cumulative_sales_in_the_month'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-27')
+        ]['company_cumulative_sales_in_the_month'].values[0] == 200
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-02-28')
+        ]['company_cumulative_sales_in_the_month'].values[0] == 280
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2022-11-09')
+        ]['company_cumulative_sales_in_the_month'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-12-09')
+        ]['company_cumulative_sales_in_the_month'].values[0] == 0
+
+
+def test_add_company_cumulative_sales_in_the_year():
+    data = {'sku': ["1", "1", "1", "1", "1", "1"],
+            'quantity': [100, 200, 80, 50, 70, 120],
+            'date': [
+                '08-02-2018', '12-02-2023',
+                '27-04-2023', '28-05-2023',
+                '09-11-2023', '09-12-2018'
+            ],
+            'warehouse': [
+                'LASCONDES', 'LASCONDES',
+                'LADEHESA', 'LADEHESA',
+                'MALLSPORT', 'MALLSPORT'
+            ]}
+    df = pd.DataFrame(data)
+    # Convert the date column to datetime type
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    df = add_company_cumulative_sales_in_the_year(df)
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2023-02-12')
+        ]['company_cumulative_sales_in_the_year'].values[0] == 0
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-04-27')
+        ]['company_cumulative_sales_in_the_year'].values[0] == 200
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LADEHESA') &
+            (df['date'] == '2023-05-28')
+        ]['company_cumulative_sales_in_the_year'].values[0] == 280
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2023-11-09')
+        ]['company_cumulative_sales_in_the_year'].values[0] == 330
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'MALLSPORT') &
+            (df['date'] == '2018-12-09')
+        ]['company_cumulative_sales_in_the_year'].values[0] == 100
+    assert \
+        df[
+            (df['sku'] == '1') &
+            (df['warehouse'] == 'LASCONDES') &
+            (df['date'] == '2018-02-08')
+        ]['company_cumulative_sales_in_the_year'].values[0] == 0
